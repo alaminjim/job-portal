827d4e1723d9cdffb2a01ed2ae2de3434f304520
alaminjim <jimalamin7@gmail.com>
2025-10-12 19:53:59 +0600

    edit

--- Frontend/src/utils/data.js
+++ Frontend/src/utils/data.js
@@ -1,4 +1,1 @@
-// export const USER_API_ENDPOINT = "http://localhost:5000/api/user";
-// export const JOB_API_ENDPOINT = "http://localhost:5000/api/job";
-// export const APPLICATION_API_ENDPOINT = "http://localhost:5000/api/application";
-// export const COMPANY_API_ENDPOINT = "http://localhost:5000/api/company";
+
