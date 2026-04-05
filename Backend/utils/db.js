1e982ced1bbc854f751e8d1f03aa05feddea46eb
Al-Amin Islam <jimalamin7@gmail.com>
2026-04-05 10:20:15 +0600

    fix buffer

--- Backend/utils/db.js
+++ Backend/utils/db.js
@@ -3,7 +3,12 @@ import mongoose from "mongoose";
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    // Set global buffering to false to prevent hanging queries on connection fail
+    mongoose.set("bufferCommands", false);
+    
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
+    });
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);
